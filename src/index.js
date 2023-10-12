f7abac0635416c44bed440fef7737080dd6ff46f
RaunakScarlet <raunakscarlet@gmail.com>
2023-10-12 13:48:49 +0530

    created nodemailer transport object

--- src/index.js
+++ src/index.js
@@ -1,16 +1,32 @@
 const express = require('express');
 
-const { ServerConfig,Logger } = require('./config');
+const { ServerConfig, Logger } = require('./config');
+
+const mailsender = require('./config/email-config');
 
 const apiRoutes = require('./routes');
 
 const app = express();
 
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+
 app.use('/api', apiRoutes);
 
-app.listen(ServerConfig.PORT, () => {
+app.listen(ServerConfig.PORT, async () => {
     console.log(`server started on PORT : ${ServerConfig.PORT}`);
-   // Logger.info('successfully started the server',{})
+    try {
+       const response=await mailsender.sendMail({
+        from: ServerConfig.GMAIL_EMAIL,
+        to: 'raunakscarlet6@gmail.com',
+        subject: 'is it working?',
+        text: 'yes, it is working.'
+    });
+    console.log(response);
+    
+   } catch (error) {
+    console.log(error);
+   }
 })
 
 
